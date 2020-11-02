06ae1cfadffae40835ac680973a0a61cfed336c1
Thomas Ploch <profiploch@gmail.com>
2020-11-02 12:36:21 +0100

    Use dynamic hosts in `wait-for-all.sh`

--- bin/wait-for-all.sh
+++ bin/wait-for-all.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-bin/wait-for-it.sh localhost:2181 -t 30 -- echo "zookeeper is up"
-bin/wait-for-it.sh localhost:9092 -t 30 -- echo "kafka broker is up"
-bin/wait-for-it.sh localhost:8081 -t 30 -- echo "schema registry is up"
+ZOOKEEPER_IPV4=${ZOOKEEPER_IPV4:-localhost}
+KAFKA_BROKER_IPV4=${KAFKA_BROKER_IPV4:-localhost}
+SCHEMA_REGISTRY_IPV4=${SCHEMA_REGISTRY_IPV4:-localhost}
+
+bin/wait-for-it.sh "${ZOOKEEPER_IPV4}:2181" -t 30 -- echo "zookeeper is up"
+bin/wait-for-it.sh "${KAFKA_BROKER_IPV4}:9092" -t 30 -- echo "kafka broker is up"
+bin/wait-for-it.sh "${SCHEMA_REGISTRY_IPV4}:8081" -t 30 -- echo "schema registry is up"
