9c89e098c0e702e06fb7a2e7dc09778b3ca2725f
Thomas Ploch <profiploch@gmail.com>
2020-11-04 06:43:34 +0100

    Increase timeout for `wait-for-it` scripts

--- bin/wait-for-all.sh
+++ bin/wait-for-all.sh
@@ -4,6 +4,6 @@ ZOOKEEPER_IPV4=${ZOOKEEPER_IPV4:-localhost}
 KAFKA_BROKER_IPV4=${KAFKA_BROKER_IPV4:-localhost}
 SCHEMA_REGISTRY_IPV4=${SCHEMA_REGISTRY_IPV4:-localhost}
 
-bin/wait-for-it.sh "${ZOOKEEPER_IPV4}:2181" -t 30 -- echo "zookeeper is up"
-bin/wait-for-it.sh "${KAFKA_BROKER_IPV4}:9092" -t 30 -- echo "kafka broker is up"
-bin/wait-for-it.sh "${SCHEMA_REGISTRY_IPV4}:8081" -t 30 -- echo "schema registry is up"
+bin/wait-for-it.sh "${ZOOKEEPER_IPV4}:2181" -t 60 -- echo "zookeeper is up"
+bin/wait-for-it.sh "${KAFKA_BROKER_IPV4}:9092" -t 60 -- echo "kafka broker is up"
+bin/wait-for-it.sh "${SCHEMA_REGISTRY_IPV4}:8081" -t 60 -- echo "schema registry is up"
